74fbdee5e1b162b3230094ab294d99666da0aa5f
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2024-03-29 09:59:08 +0000

    primeira implementação do comando e

--- project.c
+++ project.c
@@ -12,6 +12,10 @@ int N_parques = 0;
 /*Lista de parques no sistema.*/
 Parque stored_parques[MAX_PARQUES];
 
+/*Tempo atual*/
+Data data_atual;
+Hora hora_atual;
+
 
 int main () {
     processar_input();
@@ -38,7 +42,7 @@ void processar_input () {
                 processar_parques();
                 break;
             case 'e':
-                processar_entradas();
+                //processar_entradas();
                 break;
             case 's':
                 //código
@@ -223,6 +227,9 @@ int criar_parque(char nome_parque[], int capacidade, float valor_15,
     novo_parque.valor_15_apos_1hora = valor_15_apos_1hora;
     novo_parque.valor_max_diario = valor_max_diario;
 
+    novo_parque.entradas = NULL;
+    novo_parque.saidas = NULL;
+
     /*Adiciona o novo parque à lista de parques no sistema.*/
     stored_parques[N_parques++] = novo_parque;
 
@@ -241,7 +248,7 @@ void processar_parques() {
     if (n_argumentos == 0) {
         for (i = 0; i < N_parques; i++) {
             printf("%s %d %d\n", stored_parques[i].nome_parque, 
-            stored_parques[i].capacidade, stored_parques[i].capacidade);
+            stored_parques[i].capacidade, stored_parques[i].lugares_disponiveis);
         }
         return;
     } 
@@ -257,7 +264,182 @@ void processar_parques() {
 
 
 
+/*HASHTABLE DE MATRÍCULAS*/
 
+nodeHASH *cria_node_matricula(char *matricula) {
+    nodeHASH *newNode = (nodeHASH *)malloc(sizeof(nodeHASH));
+    if (newNode == NULL) {
+        perror("Memory allocation error");
+        exit(EXIT_FAILURE);
+    }
+
+    newNode->estado = DENTRO;
+    newNode->matricula = strdup(matricula);
+    newNode->next = NULL;
+    return newNode;
+}
+
+
+HashTable *cria_HashTable() {
+    HashTable *hashTable = (HashTable *)malloc(sizeof(HashTable));
+    if (hashTable == NULL) {
+        perror("Memory allocation error");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        hashTable->table[i] = NULL;
+    }
+
+    return hashTable;
+}
+
+
+unsigned long hash(char *matricula) {
+    unsigned long hash = 5381;
+    int c;
+    
+    while ((c = *matricula++)) {
+        hash = ((hash < 5) + hash) + c; /*hash * 33 + c*/
+    }
+
+    return hash % TABLE_SIZE;
+}
+
+
+/* insere_mat_hashtable antigo
+void insere_mat_hashtable(HashTable *hashTable, char *matricula) {
+    int index = hash(matricula);
+    nodeHASH *newNode = cria_node_matricula(matricula);
+
+    if (hashTable->table[index] == NULL) {
+        hashTable->table[index] = newNode;
+    }
+
+    else {
+        nodeHASH *current = hashTable->table[index];
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
+*/
+
+
+nodeHASH *procura_na_hastable(HashTable *hashTable, char *matricula) {
+    int index = hash(matricula);
+    nodeHASH *current = hashTable->table[index];
+
+    while (current != NULL) {
+        if (strcmp(current->matricula, matricula) == 0) {
+            return current;
+        }
+        current = current->next;
+    }
+
+    return NULL;
+}
+
+
+void print_hashtable(HashTable *hashTable) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        printf("[%d] -> ", i);
+        nodeHASH *current = hashTable->table[i];
+
+        while (current != NULL) {
+            printf("%s -> ", current->matricula);
+            current = current->next;
+        }
+
+        printf("NULL\n");
+    }
+}
+
+
+
+void insere_entrada_parque(Parque *parque, char *matricula, Data data, Hora hora) {
+    // Alocar memória para o novo registro
+    Registo *novo_registo = malloc(sizeof(Registo));
+    if (novo_registo == NULL) {
+        perror("Memory allocation error");
+        exit(EXIT_FAILURE);
+    }
+
+    // Preencher os dados do novo registro
+    strcpy(novo_registo->matricula, matricula); // Usando strcpy para copiar a matrícula
+    novo_registo->data = data;
+    novo_registo->hora = hora;
+    novo_registo->next = NULL;
+
+    // Verificar se a lista de entradas está vazia
+    if (parque->entradas == NULL) {
+        parque->entradas = novo_registo;
+    } else {
+        // Percorrer a lista até o último nó
+        Registo *atual = parque->entradas;
+        while (atual->next != NULL) {
+            atual = atual->next;
+        }
+        // Adicionar o novo registro no final da lista
+        atual->next = novo_registo;
+    }
+
+    // Subtrair 1 do número de lugares disponíveis
+    parque->lugares_disponiveis--;
+}
+
+
+
+void processar_entradas() {
+    char argumentos[MAX_ARGUMENTOS][MAX_INPUT];
+    int n_argumentos;
+
+    // Lê a linha de comando
+    leLinha(argumentos, &n_argumentos);
+
+    // Verifica se o número de argumentos é válido
+    if (n_argumentos == 4) {
+        char *nome_parque = argumentos[0];
+        char *matricula = argumentos[1];
+        Data data;
+        Hora hora;
+
+        // Converte a data para o formato desejado
+        sscanf(argumentos[2], "%d-%d-%d", &data.d, &data.m, &data.a);
+        
+        // Converte a hora para o formato desejado
+        sscanf(argumentos[3], "%d:%d", &hora.h, &hora.min);
+
+        // Verifica se o parque existe
+        int indice_parque = parque_existe(nome_parque);
+        if (indice_parque) {
+            // Obtém o parque
+            Parque *parque = &stored_parques[indice_parque - 1];
+            
+            // Insere a entrada no parque
+            insere_entrada_parque(parque, matricula, data, hora);
+        } else {
+            printf("%s: no such parking.\n", nome_parque);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+//vamos procurar muitas vezes as entradas e saidas dos carros
+//começar por onde da speed up e usar tabelas de dispersão
+
+//hastables para entradas e saidas
+
+
+/*
 //CÓDIGO PARA O COMANDO e
 
 int carro_existe(char matricula) {
@@ -284,13 +466,4 @@ void processar_entradas() {
 
     criar_carro(argumentos[0], argumentos[1], argumentos[2], argumentos[3]);
 }
-
-
-
-
-
-
-//vamos procurar muitas vezes as entradas e saidas dos carros
-//começar por onde da speed up e usar tabelas de dispersão
-
-//hastables para entradas e saidas
+*/
--- project.h
+++ project.h
@@ -17,26 +17,21 @@
 #define TRUE 1
 #define FALSE 0
 
+#define DENTRO 1
+#define FORA 0
+
 /*Máximo de caracteres num input*/
 #define BUFSIZ 8192
 #define MAX_INPUT BUFSIZ
+
+/*Tamanho hash table*/
+#define TABLE_SIZE 100
 
 /*Número máximo de argumentos em qualquer comando*/
 #define MAX_ARGUMENTOS 5
 
 /*Número máximo de parques permitidos no sistema*/
 #define MAX_PARQUES 20
-
-
-/*Estrutura Parque*/
-typedef struct {
-    char nome_parque[BUFSIZ];
-    int capacidade;
-    int lugares_disponiveis;
-    float valor_15;
-    float valor_15_apos_1hora;
-    float valor_max_diario; 
-} Parque;
 
 
 /*Estrutura Data (DD-MM-AAAA)*/
@@ -49,6 +44,26 @@ typedef struct {
 typedef struct {
     int h, min;
 } Hora;
+
+
+typedef struct registo {
+    char matricula[MAX_INPUT];
+    Data data;
+    Hora hora;
+    struct registo *next;
+} Registo;
+
+/*Estrutura Parque*/
+typedef struct {
+    char nome_parque[BUFSIZ];
+    int capacidade;
+    int lugares_disponiveis;
+    float valor_15;
+    float valor_15_apos_1hora;
+    float valor_max_diario;
+    Registo *entradas;
+    Registo *saidas;
+} Parque;
 
 
 /*Estrutura Carro*/
@@ -67,6 +82,18 @@ typedef struct node{
 } Node;
 
 
+/*Estrutura da Hash Table*/
+typedef struct nodeHashTable {
+    int estado;
+    char *matricula;
+    //nodeENTRADAS * ult_lugar;
+    struct nodeHashTable *next;
+} nodeHASH;
+
+typedef struct HashTable {
+    nodeHASH *table[TABLE_SIZE];
+} HashTable;
+
 
 /*Funções utilizadas*/
 void leLinha(char list_of_words[][MAX_INPUT], int *argumentos);
@@ -75,6 +102,13 @@ Node *insert_begin(Node *head, int new_id);
 Node *insert_end(Node *tail, int new_id);
 Node *remove_node(Node *head, Node *node_to_remove);
 int length(Node *head);
+
+nodeHASH *cria_node_matricula(char *matricula);
+HashTable *cria_HashTable();
+unsigned long hash(char *matricula);
+void insere_mat_hashtable(HashTable *hashTable, char *matricula, char *nome_parque, Data data, Hora hora);
+nodeHASH *procura_na_hastable(HashTable *hashTable, char *matricula);
+void print_hashtable(HashTable *hashTable);
 
 int parque_existe(char nome_parque[]);
 Parque obter_parque_por_id (int id);
